914646e310ce5276c6c47e1938b7f758467210e4
Reza Shojaei <re.shojaei@gmail.com>
2011-06-28 19:36:38 -0400

    First commit

---
+++ FileUploader_Demo/Controllers/HomeController.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.IO;
+using FileUploader_Demo.Models;
+
+namespace FileUploader_Demo.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Sample File Uploader using jquery file uploader and MVC 3";
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+
+
+        public ActionResult uploader()
+        {
+
+            switch (Request.HttpMethod)
+            {
+                case "POST":
+                    return uploader_Post();
+
+          
+                case "GET":
+                    return this.uploader_Get();
+
+            }
+
+            return Json(true);
+
+        }
+
+        public ActionResult uploader_Get()
+        {
+            String fileName = Request.Params["file"];
+
+            if (fileName == null)
+            {
+                return get_file_objects();
+            }
+            else
+            {
+                fileName = fileName.Replace("/", "");
+                return Json(true);
+
+            }
+
+        }
+
+        public ActionResult uploader_Post()
+        {
+            var files = Request.Files;
+            List<FileUploadInfo> result = new List<FileUploadInfo>();
+            for (int i = 0; i < files.Count; i++)
+            {
+                result.Add(this.handle_file_upload(files.Get(i)));
+            }
+            return Json(result);
+        }
+
+
+        private FileUploadInfo handle_file_upload(HttpPostedFileBase file)
+        {
+            FileUploadInfo info = new FileUploadInfo();
+            if (file == null)
+                throw new NullReferenceException("No file is passed");
+
+            //Save the file first 
+            String filename = Server.MapPath(UploadInfo.getUploadInfo().Upload_dir + "/" +
+                file.FileName);
+            file.SaveAs(filename);
+            // Set the info 
+            info.name = file.FileName;
+            info.size = file.ContentLength;
+            info.type = file.ContentType;
+            info.url = Url.Content(UploadInfo.getUploadInfo().Upload_dir + "/" + file.FileName);
+            info.delete_url = Url.Content("~/Home/uploader_Delete" + "/" + file.FileName);
+            info.delete_type = FileUploadInfo.TYPE_DELETE;
+            return info;
+
+        }
+
+
+        [AcceptVerbs(HttpVerbs.Delete)]
+        public ActionResult uploader_Delete(string id)
+        {
+            String fileName = id;
+            if (!String.IsNullOrEmpty(id))
+            {
+                try
+                {
+                    FileInfo f = new FileInfo(Server.MapPath(UploadInfo.getUploadInfo().Upload_dir
+                        + "/" + fileName));
+                    f.Delete();
+                }
+                catch (Exception ex)
+                {
+                    return Json(false);
+                }
+                return Json(false);
+            }
+
+            return Json(false);
+
+        }
+
+
+        private JsonResult get_file_objects()
+        {
+            String[] files = Directory.GetFiles(Server.MapPath((UploadInfo.getUploadInfo().Upload_dir)));
+
+            List<FileUploadInfo> fileList = new List<FileUploadInfo>();
+            for (int i = 0; i < files.Length; i++)
+            {
+                fileList.Add(this.get_file_object(files[i]));
+            }
+
+            return Json(fileList, JsonRequestBehavior.AllowGet);
+
+        }
+
+        private FileUploadInfo get_file_object(string p)
+        {
+            FileUploadInfo result = new FileUploadInfo();
+            try
+            {
+                FileInfo inf = new FileInfo(p);
+                result.name = inf.Name;
+                result.delete_type = FileUploadInfo.TYPE_DELETE;
+                result.size = inf.Length;
+                result.delete_url = Url.Content("~/Home/uploader_Delete" + "/" + inf.Name);
+                result.url = Url.Content(UploadInfo.getUploadInfo().Upload_dir + "/" + inf.Name);
+            }
+            catch (Exception ex)
+            {
+                result.error = "Cannot find the file";
+            }
+
+            return result;
+        }
+
+
+
+    }
+}
---
+++ FileUploader_Demo/Models/UploadInfo.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel;
+
+namespace FileUploader_Demo.Controllers
+{
+    public class UploadInfo
+    {
+
+        private static UploadInfo _upload;
+        private UploadInfo()
+        {
+            this.Upload_dir = "~/uploaded_files";
+        }
+
+        public static UploadInfo getUploadInfo()
+        {
+            if (UploadInfo._upload == null)
+                UploadInfo._upload = new UploadInfo();
+
+            return _upload;
+        }
+        private string param_name = "file";
+        private int? max_file_size = null;
+
+        [DefaultValue(null)]
+        public int? Max_file_size
+        {
+            get { return max_file_size; }
+            set { max_file_size = value; }
+        }
+        private int min_file_size = 1;
+
+        [DefaultValue(1)]
+        public int Min_file_size
+        {
+            get { return min_file_size; }
+            set { min_file_size = value; }
+        }
+        private string accept_file_types = "/.+$/";
+
+        [DefaultValue("/.+$/")]
+        public string Accept_file_types
+        {
+            get { return accept_file_types; }
+            set { accept_file_types = value; }
+        }
+        private int? max_number_of_files = null;
+
+        [DefaultValue(null)]
+        public int? Max_number_of_files
+        {
+            get { return max_number_of_files; }
+            set { max_number_of_files = value; }
+        }
+        private Boolean discard_aborted_uploads = true;
+
+        [DefaultValue(true)]
+        public Boolean Discard_aborted_uploads
+        {
+            get { return discard_aborted_uploads; }
+            set { discard_aborted_uploads = value; }
+        }
+
+        public string ScriptURL { get; set; }
+        public string Upload_dir { get; set; }
+
+
+
+        [DefaultValue("file")]
+        public string Param_name
+        {
+            get
+            {
+                return this.param_name;
+            }
+
+            set
+            {
+                this.param_name = value;
+            }
+        }
+
+    }
+
+    /*
+     * Note: Properties in this class start with small letters to 
+     * follow Json format required by FileUpload component
+     */
+    public class FileUploadInfo
+    {
+
+        public static string TYPE_DELETE = "DELETE";
+        public string name { set; get; }
+        public long size { set; get; }
+        public string url { set; get; }
+        public string delete_url { set; get; }
+        public string delete_type { set; get; }
+        public string type { set; get; }
+        public string error { set; get; }
+
+    }
+
+
+}
